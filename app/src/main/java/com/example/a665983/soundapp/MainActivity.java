f01036cbe19c7be38c9157fb06f9899f4fb4a5d4
665983 <neupaneanuj1@gmail.com>
2018-02-01 10:44:04 -0600

    not finish

--- app/src/main/java/com/example/a665983/soundapp/MainActivity.java
+++ app/src/main/java/com/example/a665983/soundapp/MainActivity.java
@@ -1,21 +1,37 @@
 package com.example.a665983.soundapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import android.media.MediaPlayer;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void message(View view) {
+        MediaPlayer piano = Mediaplayer.create(getAplicationContent(),R.raw.piano);
+        piano.start();
+        }
 
-        Intent intent = new Intent(this, secondpage.class);
+
+    public void message(View view){
+        Intent intent = new Intent(pakageContent:this, secondpage.class);
         startActivity(intent);
-    }
 }
+
+        public void email(View view){
+            Intent emailIntent = new Intent (Intent.ACTION_SEND);
+            //Bellow adds to the email to me
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA _EMAIL,new String{}{"neupaneanuj1@gmail.com"));//recipients
+        emailIntent.putExtra(Intent.EXTRA _SUBJECT,  value: "Sound APP");
+        emailIntent.putExtra(Intent.EXTRA _TEXT, value "listen to my voice");
+
+        startActivity(emailIntent);
+
+
+    }
+        }
